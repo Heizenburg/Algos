7f6bd90f47ff596b40c9c4396f8263898f4e970b
Thato Sello <thatokagisosello@gmail.com>
2021-09-27 11:48:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1])
+        [array[j], array[j + 1]] = [array[j + 1], array[j]]; 
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([58, 58, 98, 516, 4, 8959, 345, 45, 97, 47]));
